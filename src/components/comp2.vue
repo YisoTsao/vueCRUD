f8494fdd225e76278bd0aa595819630134a6dd21
gary.tsao <gary.tsao@car-plus.com.tw>
2020-02-05 15:04:54 +0800

    add sementic ui

---
+++ src/components/comp2.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <sui-button color="red" content="Like" icon="heart">
+      <a is="sui-label" slot="label" basic color="red" pointing="left">
+        2,048
+      </a>
+    </sui-button>
+
+    <sui-button basic color="blue" content="Fork" icon="fork">
+      <a is="sui-label" slot="label" basic color="blue" pointing="left">
+        1,048
+      </a>
+    </sui-button>
+  </div>
+
+
+</template>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
+import store from './store/index'
+import router from './store/router'
+import 'semantic-ui-css/semantic.min.css';
+import SuiVue from 'semantic-ui-vue';
+
+Vue.use(SuiVue);
 
 Vue.config.productionTip = false
 
+
+
 new Vue({
+  el: '#app',
+  router,
   store,
   render: h => h(App)
 }).$mount('#app')
